50c5bc93988b1b9051769c30fc898b5309b1833b
RayanAlHassan <rayanalhassan2000@gmail.com>
2023-09-22 11:56:47 +0300

    step 2

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("h");
+const express = require("express"); // get express package
+const app = express(); // to make l app work with express
+
+app.get("/", (req, res) => {
+  res.send("OK");
+  console.log("here");
+});
+
+app.listen(3000, () => {
+  console.log("run server");
+}); // give the port that we gonna opent in and  make function to test if server open
