fb980863e1eba7bb6d83605c3206067a7b7495c8
RayanAlHassan <rayanalhassan2000@gmail.com>
2023-09-22 12:27:18 +0300

    step 3

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require("express"); // get express package
 const app = express(); // to make l app work with express
+const today = new Date();
 
 app.get("/", (req, res) => {
-  res.send("OK");
+  res.send("OKkk");
   console.log("here");
 });
-
+app.get("/test", (req, res) => {
+  res.status(200).send("ok"); // if we want to send message with status we use .status , but if we want just send status we use .sendStatus
+});
+app.get("/time", (req, res) => {
+  var time = today.getHours() + ":" + today.getMinutes();
+  res.status(200).send(time);
+});
 app.listen(3000, () => {
   console.log("run server");
 }); // give the port that we gonna opent in and  make function to test if server open
