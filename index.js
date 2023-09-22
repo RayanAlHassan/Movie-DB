fc7adfc3ffbbb2f573d14d8e2dfef710be8d21a8
RayanAlHassan <rayanalhassan2000@gmail.com>
2023-09-22 16:39:52 +0300

    step 4 and adjust json in previous steps

--- index.js
+++ index.js
@@ -7,11 +7,28 @@ app.get("/", (req, res) => {
   console.log("here");
 });
 app.get("/test", (req, res) => {
-  res.status(200).send("ok"); // if we want to send message with status we use .status , but if we want just send status we use .sendStatus
+  res.status(200).json({ status: 200, message: "ok" }); // if we want to send message with status we use .status , but if we want just send status we use .sendStatus
 });
 app.get("/time", (req, res) => {
   var time = today.getHours() + ":" + today.getMinutes();
-  res.status(200).send(time);
+  res.status(200).json({ status: 200, time: time });
+});
+
+app.get("/hello/:id?/", (req, res) => {
+  let id = req.params.id || "Fadi";
+  let statuss = res.status(200);
+  res.status(200).json({ status: 200, message: "hello ", id: id });
+});
+app.get("/search", (req, res) => {
+  const search = req.query.s;
+  if (search) {
+    res.status(200).json({ status: 200, message: "ok", data: search });
+  } else
+    res.status(500).json({
+      status: 500,
+      error: true,
+      message: "you have to provide a search",
+    });
 });
 app.listen(3000, () => {
   console.log("run server");
